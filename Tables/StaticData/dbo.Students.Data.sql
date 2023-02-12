24af2e0e01e2e19542e8d17aabd3357e27adb1ef
Mostafa <mostafaelnady1997@gmail.com>
2023-02-12 15:38:26 +0200

    test1

---
+++ Tables/StaticData/dbo.Students.Data.sql
@@ -0,0 +1,6 @@
+﻿SET IDENTITY_INSERT dbo.Students ON
+GO
+INSERT dbo.Students(st_id, fname, lname, email, city, governorate, address, phone) VALUES (1, 'mostafa', 'mohamed', 'mostafaelnady1997@gmail.com', 'zagzig', 'sharkia', N'kawmia', 1212806000);
+GO
+SET IDENTITY_INSERT dbo.Students OFF
+GO
