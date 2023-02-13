da949f92a085c02ae709a488bf6ac87bf7db3de8
Mostafa <mostafaelnady1997@gmail.com>
2023-02-13 23:11:56 +0200

    data added

---
+++ Tables/StaticData/dbo.Courses.Data.sql
@@ -0,0 +1,47 @@
+﻿SET IDENTITY_INSERT dbo.Courses ON
+GO
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (1, 'HTML', 18);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (2, 'CSS', 25);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (3, 'JS', 30);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (4, 'RWD', 15);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (5, 'C#', 30);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (6, 'XML', 20);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (7, 'Advanced JS', 20);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (8, 'UML', 20);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (9, 'SQL', 20);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (10, 'NodeJS', 20);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (11, 'MongoDB', 16);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (12, 'XHTML', 15);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (13, 'Advanced SQL', 18);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (14, 'MySQL', 10);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (15, 'JAVA', 16);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (16, 'typescript', 15);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (17, 'Angular', 15);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (18, 'OOP', 20);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (19, 'LINQ', 16);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (20, 'EF', 15);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (21, 'C++', 16);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (22, 'HTML', 18);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (23, 'CSS', 25);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (24, 'JS', 30);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (25, 'RWD', 15);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (26, 'C#', 30);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (27, 'XML', 20);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (28, 'Advanced JS', 20);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (29, 'UML', 20);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (30, 'SQL', 20);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (31, 'NodeJS', 20);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (32, 'MongoDB', 16);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (33, 'XHTML', 15);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (34, 'Advanced SQL', 18);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (35, 'MySQL', 10);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (36, 'JAVA', 16);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (37, 'typescript', 15);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (38, 'Angular', 15);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (39, 'OOP', 20);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (40, 'LINQ', 16);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (41, 'EF', 15);
+INSERT dbo.Courses(curs_id, curs_name, duration) VALUES (42, 'C++', 16);
+GO
+SET IDENTITY_INSERT dbo.Courses OFF
+GO
---
+++ Tables/StaticData/dbo.Departments.Data.sql
@@ -0,0 +1,17 @@
+﻿SET IDENTITY_INSERT dbo.Departments ON
+GO
+INSERT dbo.Departments(dep_id, dep_name, dep_mgr) VALUES (1, 'PD', NULL);
+INSERT dbo.Departments(dep_id, dep_name, dep_mgr) VALUES (2, 'OS', NULL);
+INSERT dbo.Departments(dep_id, dep_name, dep_mgr) VALUES (3, 'AI', NULL);
+INSERT dbo.Departments(dep_id, dep_name, dep_mgr) VALUES (4, 'BI', NULL);
+INSERT dbo.Departments(dep_id, dep_name, dep_mgr) VALUES (5, 'UI', NULL);
+INSERT dbo.Departments(dep_id, dep_name, dep_mgr) VALUES (6, 'UX', NULL);
+INSERT dbo.Departments(dep_id, dep_name, dep_mgr) VALUES (7, 'PD', NULL);
+INSERT dbo.Departments(dep_id, dep_name, dep_mgr) VALUES (8, 'OS', NULL);
+INSERT dbo.Departments(dep_id, dep_name, dep_mgr) VALUES (9, 'AI', NULL);
+INSERT dbo.Departments(dep_id, dep_name, dep_mgr) VALUES (10, 'BI', NULL);
+INSERT dbo.Departments(dep_id, dep_name, dep_mgr) VALUES (11, 'UI', NULL);
+INSERT dbo.Departments(dep_id, dep_name, dep_mgr) VALUES (12, 'UX', NULL);
+GO
+SET IDENTITY_INSERT dbo.Departments OFF
+GO
